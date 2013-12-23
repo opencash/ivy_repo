1b60accb3cba989a2cb8f37a83080388b619157b
Serge Gebhardt <serge.gebhardt@gmail.com>
2013-12-23 18:41:17 -0200

    index: ignore .git directory

--- update-directory-index.sh
+++ update-directory-index.sh
@@ -6,7 +6,7 @@
 for DIR in $(find . -type d \( -name ".git" -prune -o -print \)); do
   (
     echo -e "<html>\n<body>\n<h1>Directory listing</h1>\n<hr/>\n<pre>"
-    ls -1pa "${DIR}" | grep -v "^\./$" | grep -v "^index\.html$" \
+    ls -1pa "${DIR}" | grep -v "^\./$" | grep -v "^\.git/$" | grep -v "^index\.html$" \
         | awk '{ printf "<a href=\"%s\">%s</a>\n",$1,$1 }'
     echo -e "</pre>\n</body>\n</html>"
   ) > "${DIR}/index.html"
